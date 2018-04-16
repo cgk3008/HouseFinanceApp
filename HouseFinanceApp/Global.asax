a3b3f8d5dc404acf42b7fc1f9e6de5a10f96a92d
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-04-16 10:45:03 -0400

    Add project files.

---
+++ HouseFinanceApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HouseFinanceApp.MvcApplication" Language="C#" %>
---
+++ HouseFinanceApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HouseFinanceApp.Startup))]
+namespace HouseFinanceApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
