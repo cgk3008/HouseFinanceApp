28024bc9312eb0a735ca6f3e1e4adec395459f27
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-06-14 13:51:21 -0400

    minor changes to table labels, starting to add calculations for balances minus transactions. Started adding unit test case. Will want that for transactions to check reconciledBalance does not go negative when adding new transaction and reconciling the transaction.

---
+++ HouseFinanceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HouseFinanceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("HouseFinanceTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c853856-60b0-48a3-a7d0-a1fc273bf10f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
